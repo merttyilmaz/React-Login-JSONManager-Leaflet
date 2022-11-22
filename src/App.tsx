c7c8fb91896abb6aa7b0a0f9e32435889bf5f7b1
Mert Yılmaz <myyilmaz97@gmail.com>
2022-11-22 12:17:47 +0300

    Completed public and private routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 
 import Loading from "@/common/components/loading";
-import PrivateRoutes from "./util/PrivateRoute";
+import { useAuthStore } from "./store/auth";
 
 const LoginPage = lazy(() => import("@/pages/login/Login"));
 const DevivesPage = lazy(() => import("@/pages/devices/Devices"));
@@ -15,19 +15,29 @@ const MapPage = lazy(() => import("@/pages/map/Map"));
 const AlarmPage = lazy(() => import("@/pages/alarm/Alarm"));
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { isLoggedIn } = useAuthStore();
+
+  if (!isLoggedIn) {
+    return (
+      <Router>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    );
+  }
 
   return (
     <Router>
       <Suspense fallback={<Loading />}>
         <Routes>
-          <Route element={<PrivateRoutes />}>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/devices" element={<DevivesPage />} />
-            <Route path="/map" element={<MapPage />} />
-            <Route path="/alarm" element={<AlarmPage />} />
-            <Route path="*" element={<Navigate to="/login" replace />} />
-          </Route>
+          <Route path="/devices" element={<DevivesPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/alarm" element={<AlarmPage />} />
+          <Route path="*" element={<Navigate to="/devices" replace />} />
         </Routes>
       </Suspense>
     </Router>
